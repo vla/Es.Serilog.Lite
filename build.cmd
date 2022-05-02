952a4c8c0a158f5fbb0eb3441ed4a424aac5a53f
v.la <v.la@live.cn>
2022-05-02 10:11:07 +0800

    chore: change version to 0.3.0

--- build.cmd
+++ build.cmd
@@ -1,9 +1,10 @@
 set artifacts=%~dp0artifacts
+set /p ver=<VERSION
 
 if exist %artifacts%  rd /q /s %artifacts%
 
 call dotnet restore src/Literate
 
-call dotnet pack src/Literate -c release -o %artifacts%
+call dotnet pack src/Literate -c release -p:Ver=%ver%  -o %artifacts%
 
 pause
